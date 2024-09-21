d3bfb69f509b050650ecb42cd39313ce0f0e1158
Aleksei Trofimov <traf72@gmail.com>
2024-09-21 10:57:17 +0300

    Lesson 4

---
+++ Lesson_004__ClassLibrary/Class1.cs
@@ -0,0 +1,10 @@
+﻿namespace ClassLibrary
+{
+    public class Class1
+    {
+        public static string ToTitleCase(string s)
+        {
+            return char.ToUpper(s[0]) + s.Substring(1);
+        }
+    }
+}
---
+++ Lesson_004__ConsoleApp/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ClassLibrary;
+
+Console.WriteLine("Hello from first app!");
+Console.WriteLine(Class1.ToTitleCase("alex"));
+Console.ReadLine();
