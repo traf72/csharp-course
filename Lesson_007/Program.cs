47e29a4c88a115d505852c59265a0d30f6d115d0
Aleksei Trofimov <traf72@gmail.com>
2024-10-07 21:47:47 +0300

    Lesson 7

---
+++ Lesson_007/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Lesson7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Args length: {args.Length}");
+            foreach (string item in args)
+            {
+                Console.WriteLine(item);
+            }
+
+            int sum = Add(5, 6);
+            Console.WriteLine(sum);  // 11
+        }
+
+        static int Add(int x, int y)
+        {
+            int result = x + y;
+            return result;
+        }
+    }
+}
